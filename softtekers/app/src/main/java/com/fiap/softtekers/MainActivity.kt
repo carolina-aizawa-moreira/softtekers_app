1a5b53b2231014c926b8d8b6823c1f6b932f5b0a
TECGRAF\gbello <gbello@tecgraf.puc-rio.br>
2025-09-16 11:54:57 -0300

    Workflow das telas, tema do aplicacao, estrutura do app

--- softtekers/app/src/main/java/com/fiap/softtekers/MainActivity.kt
+++ softtekers/app/src/main/java/com/fiap/softtekers/MainActivity.kt
@@ -1,47 +1,91 @@
 package com.fiap.softtekers
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.ScaffoldDefaults.contentWindowInsets
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.fiap.softtekers.components.BottomNavBar
+import com.fiap.softtekers.components.TopNavBar
+import com.fiap.softtekers.screens.AnalisysScreen
+import com.fiap.softtekers.screens.CheckInScreen
+import com.fiap.softtekers.screens.HomeScreen
+import com.fiap.softtekers.screens.LoginScreen
 import com.fiap.softtekers.ui.theme.SofttekersTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SofttekersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                // Observe the current back stack entry
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+                val showTopBar = when (currentRoute) {
+                    "home", "checkIn", "analisys" -> true // Show on these screens
+                    else -> false // Hide on other screens (e.g., "login")
+                }
+                val showBottomBar = when (currentRoute) {
+                    "home", "analisys" -> true // Show on these screens
+                    else -> false // Hide on other screens (e.g., "login")
+                }
+                Scaffold(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    contentWindowInsets = contentWindowInsets,
+                    topBar = {
+                        if (showTopBar) {
+                            TopNavBar(navController = navController)
+                        }
+                    },
+                    bottomBar = {
+                        if (showBottomBar) {
+                            BottomNavBar(navController = navController)
+                        }
+                    }
+                ) { innerPadding: PaddingValues ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                        modifier = Modifier
+                            .fillMaxSize() // Fill the available space
+                            .padding(innerPadding) // <-- APPLY the PaddingValues here
+                    ) {
+
+                        composable("login") {
+                            LoginScreen(navController)
+                        }
+                        composable("home") {
+                            HomeScreen(navController)
+                        }
+                        composable("checkIn") {
+                            CheckInScreen(navController)
+                        }
+                        composable("analisys"){
+                            AnalisysScreen(navController)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SofttekersTheme {
-        Greeting("Android")
     }
 }
---
+++ softtekers/app/src/main/java/com/fiap/softtekers/screens/LoginScreen.kt
@@ -0,0 +1,40 @@
+package com.fiap.softtekers.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.fiap.softtekers.ui.theme.YouTertiary
+
+@Composable
+
+fun LoginScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(MaterialTheme.colorScheme.surface.value))
+            .padding(20.dp)
+    ) {
+        Text(text = "Você em primeiro lugar.", style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.tertiary)
+        Text(text = "Programa de apoio à saúde mental e psicossocial dos Softtekers")
+        Button(
+            onClick = {
+                navController.navigate("checkIn")
+            },
+            colors = ButtonDefaults.buttonColors(YouTertiary)
+        ) {
+            Text(text = "Entrar")
+        }
+    }
+}
