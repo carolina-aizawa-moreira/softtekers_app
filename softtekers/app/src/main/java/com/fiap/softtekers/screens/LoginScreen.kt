913b3bd9ccc6f80400be6f7dbccbe654c32380d3
TECGRAF\gbello <gbello@tecgraf.puc-rio.br>
2025-10-02 10:49:35 -0300

    Tela de login

--- softtekers/app/src/main/java/com/fiap/softtekers/screens/LoginScreen.kt
+++ softtekers/app/src/main/java/com/fiap/softtekers/screens/LoginScreen.kt
@@ -1,18 +1,26 @@
 package com.fiap.softtekers.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.fiap.softtekers.R
 import com.fiap.softtekers.ui.theme.YouTertiary
 
 @Composable
@@ -22,17 +30,44 @@ fun LoginScreen(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color(MaterialTheme.colorScheme.surface.value))
-            .padding(20.dp)
+            .paint(  painterResource(id = R.drawable.background), contentScale = ContentScale.Crop)
     ) {
-        Text(text = "Você em primeiro lugar.", style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.tertiary)
-        Text(text = "Programa de apoio à saúde mental e psicossocial dos Softtekers")
-        Button(
-            onClick = {
-                navController.navigate("checkIn")
-            },
-            colors = ButtonDefaults.buttonColors(YouTertiary)
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
         ) {
-            Text(text = "Entrar")
+            Image(
+            painter = painterResource(id = R.drawable.softtek_brand),
+                contentDescription = "You Brand",
+                modifier = Modifier.width(120.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.you_brand),
+                contentDescription = "You Brand"
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Text(text = "Você em primeiro lugar.",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.tertiary,
+
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(
+                text = "Programa de apoio à saúde mental e psicossocial dos Softtekers",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White,
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(
+                onClick = {
+                    navController.navigate("checkIn")
+                },
+                colors = ButtonDefaults.buttonColors(YouTertiary)
+            ) {
+                Text(text = "Entrar")
+            }
         }
     }
 }
--- softtekers/app/src/main/java/com/fiap/softtekers/ui/theme/Type.kt
+++ softtekers/app/src/main/java/com/fiap/softtekers/ui/theme/Type.kt
@@ -22,30 +22,37 @@ val HankenGrotesk = FontFamily(
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = HankenGrotesk,
-        fontWeight = FontWeight.Medium,
-        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     titleLarge = TextStyle(
         fontFamily = HankenGrotesk,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
         fontFamily = HankenGrotesk,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 48.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
+    bodySmall = TextStyle(
         fontFamily = HankenGrotesk,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = HankenGrotesk,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
 )
